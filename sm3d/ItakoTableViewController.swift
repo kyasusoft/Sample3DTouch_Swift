03cc880ffbacb5bf745a6f3d50543542b717ff51
kyasu <kyasu@mac.com>
2016-01-24 01:26:25 +0900

    cellのtagを使わない。

--- sm3d/ItakoTableViewController.swift
+++ sm3d/ItakoTableViewController.swift
@@ -19,9 +19,9 @@ class ItakoTableViewController: UITableViewController, UIViewControllerPreviewin
         // 3D Touchに反応するビューを登録（iOS 9以降でforceTouch有り）
         if atof(UIDevice.currentDevice().systemVersion) >= 9.0 {
             if traitCollection.forceTouchCapability == .Available {
-                // 1番目のcellを登録(cellのtag:0)
+                // 1番目のcellを登録
                 registerForPreviewingWithDelegate(self, sourceView: cell0)
-                // 2番目のcellを登録(cellのtag:1)
+                // 2番目のcellを登録
                 registerForPreviewingWithDelegate(self, sourceView: cell1)
             }
         }
@@ -36,8 +36,13 @@ class ItakoTableViewController: UITableViewController, UIViewControllerPreviewin
     
     // 3D Touch peek（peekで表示するViewControllerを返却する）
     func previewingContext(previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
-        // cellのtagによりイタコ１かイタコ２のviewControllerを返却する
-        if previewingContext.sourceView.tag == 0 {
+        
+        if let indexPath = tableView.indexPathForRowAtPoint(location) {
+            // タッチしたcellのrectを設定する
+            previewingContext.sourceRect = tableView.rectForRowAtIndexPath(indexPath)
+        }
+        // イタコ１かイタコ２のviewControllerを返却する
+        if previewingContext.sourceView == cell0 {
             return self.storyboard!.instantiateViewControllerWithIdentifier("view0")
         } else {
             return self.storyboard!.instantiateViewControllerWithIdentifier("view1")
@@ -46,8 +51,8 @@ class ItakoTableViewController: UITableViewController, UIViewControllerPreviewin
 
     // 3D Touch pop（pop先のViewControllerに遷移する）
     func previewingContext(previewingContext: UIViewControllerPreviewing, commitViewController viewControllerToCommit: UIViewController) {
-        // cellのtagによりイタコ１かイタコ２の詳細画面に遷移する
-        if previewingContext.sourceView.tag == 0 {
+        // イタコ１かイタコ２の詳細画面に遷移する
+        if previewingContext.sourceView == cell0 {
             performSegueWithIdentifier("showView0", sender:nil)
         } else {
             performSegueWithIdentifier("showView1", sender:nil)
